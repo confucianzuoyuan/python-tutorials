3ac39e02050ac355c68b27241c49836698269049
confucianzuoyuan <zuoyuantc@126.com>
2018-05-05 11:23:36 +0800

    add

--- tornadotutorial/messageboard/urls.py
+++ tornadotutorial/messageboard/urls.py
@@ -4,7 +4,6 @@ from handlers.BaseHandler import StaticFileBaseHandler as StaticFileHandler
 from handlers import MessageBoard
 
 urls = [
-    # (r"/log", Passport.LogHandler),
     (r"/api/submit", MessageBoard.MessageHandler),
     (r"/(.*)", StaticFileHandler,
      dict(path=os.path.join(os.path.dirname(__file__), "html"), default_filename="messageboard.html"))
